837cd61decc5d9555ead0dc9155d9031e27c0add
kk1111688 <kk1111688@github.com>
2026-07-28 01:09:18 +0800

    fix: 修复PWA路径配置，解决桌面图标404问题

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'fitfood-v1';
+const CACHE_NAME = 'fitfood-v2';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/favicon.svg'
+  './',
+  './index.html',
+  './manifest.json',
+  './favicon.svg'
 ];
 
 self.addEventListener('install', (event) => {
